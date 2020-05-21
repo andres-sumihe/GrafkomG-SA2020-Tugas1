457d79a07a23b7fc54956ad2365462271547b003
andres sumihe <672018136@student.uksw.edu>
2020-05-21 22:26:48 +0700

    Add 3D Chair

--- GrafkomG-SA2020-Tugas1/GrafkomG-SA2020-Tugas1.cpp
+++ GrafkomG-SA2020-Tugas1/GrafkomG-SA2020-Tugas1.cpp
@@ -35,13 +35,23 @@ void Rumah() {
 
 	glEnd();
 	
-	glColor3ub(0, 0, 0);
-	glBegin(GL_LINE_LOOP);
+	//Cerobong
+	glColor3ub(143, 87, 41);
+	glBegin(GL_POLYGON);
 	glVertex2f(264, 150);
 	glVertex2f(264, 186);
 	glVertex2f(250, 194);
 	glVertex2f(250, 150);
 	glEnd();
+
+	glColor3ub(139, 69, 19);
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(265, 149);
+	glVertex2f(265, 187);
+	glVertex2f(251, 195);
+	glVertex2f(251, 149);
+	glEnd();
+	//Akhir Cerobong
 	
 	glColor3ub(139, 69, 19);
 	glBegin(GL_POLYGON);
@@ -122,6 +132,55 @@ void Rumah() {
 
 		
 	}
+
+	//Kursi
+	glColor3ub(139, 69, 19);
+	glBegin(GL_POLYGON);
+	glVertex2f(110, 50);
+	glVertex2f(110, 85);
+	glVertex2f(115, 80);
+	glVertex2f(115, 45);
+	glEnd();
+	
+	glColor3ub(139, 69, 19);
+	glBegin(GL_POLYGON);
+	glVertex2f(77, 37);
+	glVertex2f(110, 50);
+	glVertex2f(115, 45);
+	glVertex2f(82, 32);
+	glEnd();
+	
+	glColor3ub(139, 69, 19);
+	glBegin(GL_POLYGON);
+	glVertex2f(77, 37);
+	glVertex2f(110, 50);
+	glVertex2f(110, 85);
+	glVertex2f(77, 71);
+	glEnd();
+	
+	glColor3ub(56, 129, 245);
+	glBegin(GL_POLYGON);
+	glVertex2f(45, 90);
+	glVertex2f(77, 104);
+	glVertex2f(110, 85);
+	glVertex2f(77, 71);
+	glEnd();
+	
+	glColor3ub(56, 129, 245);
+	glBegin(GL_POLYGON);
+	glVertex2f(45, 90);
+	glVertex2f(77, 104);
+	glVertex2f(77, 109);
+	glVertex2f(45, 95);
+	glEnd();
+	
+	glColor3ub(56, 129, 245);
+	glBegin(GL_POLYGON);
+	glVertex2f(77, 109);
+	glVertex2f(77, 104);
+	glVertex2f(115, 80);
+	glVertex2f(115, 85);
+	glEnd();
 	
 
 	glColor3ub(0, 0, 0);
